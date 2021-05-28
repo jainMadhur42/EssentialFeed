20fd2a6e5842d7054265fd599d6fd2c19c3e7ecb
Madhur Jain <jmadhur.kothadar42@gmail.com>
2021-05-29 01:54:42 +0900

    Create Essential Feed Item Project

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Madhur Jain on 29/05/21.
+//
+
+import Foundation
+
+struct FeedItem {
+	let id: UUID
+	let description: String?
+	let location: String?
+	let url: URL
+}
