83961223c8b631e372ac76bdccd0930c803beef0
Madhur Jain <jmadhur.kothadar42@gmail.com>
2021-05-30 00:03:38 +0900

    1. Deliver Connectivity Error on client error 2. Remove unused file

---
+++ EssentialFeed/FeedApi/RemoteFeedLoader.swift
@@ -0,0 +1,31 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Madhur Jain on 29/05/21.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+	func get(from url: URL, completion: @escaping (Error) -> Void)
+}
+
+public final class RemoteFeedLoader {
+	private let client: HTTPClient
+	private let url: URL
+	
+	public enum Error: Swift.Error {
+		case connectivity
+	}
+	
+	public init(url: URL, client: HTTPClient) {
+		self.client = client
+		self.url = url
+	}
+	public func load(completion: @escaping (Error) -> Void = {_ in }) {
+		client.get(from: url) { error in
+			completion(.connectivity)
+		}
+	}
+}
--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -6,42 +6,34 @@
 //
 
 import XCTest
-
-class RemoteFeedLoader {
-	let client: HTTPClient
-	let url: URL
-	
-	init(url: URL, client: HTTPClient) {
-		self.client = client
-		self.url = url
-	}
-	func load() {
-		client.get(from: url)
-	}
-}
-
-protocol HTTPClient {
-	func get(from: URL)
-}
+@testable import EssentialFeed
 
 class RemoteFeedLoaderTests: XCTestCase {
 
-	
-	
 	func test_init_doesNotRequestDataFromURL() {
-		let (sut, client) = makeSUT()
-		XCTAssertNil(client.requestedURL)
+		let (_, client) = makeSUT()
+		XCTAssertTrue(client.requestedURL.isEmpty)
 	}
 	
 	func test_load_requestDataFromURL() {
 		let url = URL(string: "https://a-madhur.gmail.com")!
 		let (sut, client) = makeSUT()
+		
 		sut.load()
-		XCTAssertEqual(client.requestedURL, url)
+		sut.load()
+		
+		XCTAssertEqual(client.requestedURL, [url,url])
+	}
+	
+	func test_load_deliversErrorOnClientError() {
+		let (sut, client) = makeSUT()
+		client.error = NSError(domain: "Test", code: 0)
+		var capturedError: RemoteFeedLoader.Error?
+		sut.load { error in capturedError = error }
+		XCTAssertEqual(capturedError, .connectivity)
 	}
 	
 	// MARK: - Helper
-	
 	private func makeSUT(url: URL = URL(string: "https://a-madhur.gmail.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
 		let client = HTTPClientSpy()
 		let sut = RemoteFeedLoader(url: url, client: client)
@@ -49,10 +41,14 @@ class RemoteFeedLoaderTests: XCTestCase {
 	}
 	
 	class HTTPClientSpy: HTTPClient {
-		var requestedURL: URL?
-		func get(from url: URL) {
-			requestedURL = url
+		func get(from url: URL, completion: @escaping (Error) -> Void) {
+			if let error = error {
+				completion(error)
+			}
+			requestedURL.append(url)
 		}
+		
+		var requestedURL = [URL]()
+		var error: Error?
 	}
-	
 }
