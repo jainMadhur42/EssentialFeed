ea1a6bce400603b8138caaed3a9931cac2e9fca3
Madhur Jain <jmadhur.kothadar42@gmail.com>
2021-05-29 21:43:38 +0900

    more generic version of HTTPclient

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -10,26 +10,36 @@ import XCTest
 class RemoteFeedLoader {
 	
 	func load() {
-		HTTPClient.shared.requestedURL = URL(string: "https://a-madhur.gmail.com")
+		HTTPClient.shared.get(from: URL(string: "https://a-madhur.gmail.com")!)
 	}
 }
 
 class HTTPClient {
-	static let shared = HTTPClient()
-	private init() {}
+	static var shared = HTTPClient()
+	func get(from: URL) { }
 	var requestedURL: URL?
+}
+
+
+class HTTPClientSpy: HTTPClient {
+	
+	override func get(from url: URL) {
+		requestedURL = url
+	}
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
 	
 	func test_init_doesNotRequestDataFromURL() {
 		_ = RemoteFeedLoader()
-		let client = HTTPClient.shared
+		let client = HTTPClientSpy()
+		HTTPClient.shared = client
 		XCTAssertNil(client.requestedURL)
 	}
 	
 	func test_load_requestDataFromURL() {
-		let client = HTTPClient.shared
+		let client = HTTPClientSpy()
+		HTTPClient.shared = client
 		let sut = RemoteFeedLoader()
 		sut.load()
 		XCTAssertNotNil(client.requestedURL)
