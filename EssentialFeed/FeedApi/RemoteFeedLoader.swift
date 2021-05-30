09365e830cf79a599b70911de7fb006f3df7512a
Madhur Jain <jmadhur.kothadar42@gmail.com>
2021-05-30 13:00:19 +0900

    move decoding logic private item struct to decouple FeedItem from Item model.

--- EssentialFeed/FeedApi/RemoteFeedLoader.swift
+++ EssentialFeed/FeedApi/RemoteFeedLoader.swift
@@ -39,7 +39,9 @@ public final class RemoteFeedLoader {
 			switch result {
 			case let .success(data, response):
 				if response.statusCode == 200 , let root = try? JSONDecoder().decode(Root.self, from: data) {
-					completion(.success(root.items))
+					completion(.success(root.items.map(
+						{ $0.item }))
+					)
 				} else {
 					completion(.failure(.invalidData))
 				}
@@ -52,5 +54,16 @@ public final class RemoteFeedLoader {
 }
 
 private struct Root: Decodable {
-	let items: [FeedItem]
+	let items: [item]
+}
+
+private struct item: Decodable {
+	public let id: UUID
+	public let description: String?
+	public let location: String?
+	public let image: URL
+
+	var item: FeedItem {
+		FeedItem(id: id, description: description, location: location, imageURL: image)
+	}
 }
